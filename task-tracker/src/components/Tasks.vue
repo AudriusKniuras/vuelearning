ce7781dbe5c0b34dcbfd48c12c4a83e4850f3610
Audrius Kniuras <audrius.kniuras@tesonet.com>
2021-10-23 21:53:22 +0300

    init learning

---
+++ task-tracker/src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <!-- emit task.id to upper level  -->
+    <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array
+  },
+  components: {
+    Task,
+  },
+  emits: ['delete-task']
+}
+</script>
